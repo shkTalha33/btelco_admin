e6036c35f65a1b52d091635288912c34d75eb3b1
shkTalha33 <shykhtalha33@gmail.com>
2025-02-15 16:08:02 +0500

    remove logging of token in file upload function

--- src/components/api/uploadFile.jsx
+++ src/components/api/uploadFile.jsx
@@ -5,7 +5,6 @@ import { axiosInstance } from "./axiosInstance";
 import { imageUpload } from "./ApiRoutesFile";
 export const uploadFile = async (file) => {
   const token = localStorage.getItem('btelco_admin_panel');
-  console.log("token",token)
   const header = {
     "Content-Type": "multipart/form-data",
     "x-auth-token": token,
